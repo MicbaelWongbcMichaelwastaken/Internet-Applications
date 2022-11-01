5c236b1ab4acddc3c3d61f89b70204d08b55a975
MicbaelWongbcMichaelwastaken <h.michaelwong@gmail.com>
2022-10-31 21:35:34 -0400

    Update weather.php

--- Weather Report/weather.php
+++ Weather Report/weather.php
@@ -1,6 +1,6 @@
 <?php
 $zip=$_GET["zip"];
-$url="http://api.openweathermap.org/data/2.5/weather?zip=$zip,us&units=imperial&APPID=dde48d23fde6be800380a91e020499fb";
+$url="http://api.openweathermap.org/data/2.5/weather?zip=$zip,us&units=imperial&APPID=";
 $file = fopen($url,"r");
 $contents = "";
 while($more = fread($file,1000)){
